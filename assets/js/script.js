0c203d1a08566d36b16e5f1712f0fae1148cad82
Cristobal Marquez-Glynn <cristobalmqz@gmail.com>
2022-04-16 22:06:14 -0700

    Added comments to HTML, JS, and CSS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Event listener for the side navigation when screen is less than 992px
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
